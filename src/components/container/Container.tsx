cd54644f160d72a89a42f55c0c1f597fddb6f723
Stuart Hendren <stuart@committed.io>
2019-08-20 15:17:39 +0100

    Add Container components

---
+++ src/components/container/Container.tsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+import MaterialContainer, {
+  ContainerProps as MaterialContainerProps
+} from "@material-ui/core/Container"
+
+export type ContainerProps = MaterialContainerProps
+
+export const Container: React.ComponentType<ContainerProps> = MaterialContainer
+Container.displayName = "Container"
+
+export default Container as React.FC<ContainerProps>
