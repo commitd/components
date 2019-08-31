15257efaed3f54ab1a7f8c40dc70f977beb7d9f8
Stuart Hendren <stuart@committed.io>
2019-08-31 15:01:16 +0100

    Correct AppBar to use theme

--- src/components/appbar/AppBar.tsx
+++ src/components/appbar/AppBar.tsx
@@ -1,18 +1,19 @@
 import * as React from "react"
 import { styled } from "@material-ui/styles"
-import { fontSizes, fontWeights } from "../../theme/theme"
+import { fonts } from "../../theme"
+import { Theme } from "@material-ui/core/styles/createMuiTheme"
 import MaterialAppBar, {
   AppBarProps as MaterialAppBarProps
 } from "@material-ui/core/AppBar"
 
 export type AppBarProps = MaterialAppBarProps
 
-export const AppBar = styled(MaterialAppBar)({
+export const AppBar = styled(MaterialAppBar)(({ theme }: { theme: Theme }) => ({
   "& h1, h2, h3, h4, h5, h6": {
-    fontSize: fontSizes[1],
-    fontWeight: fontWeights.medium
+    fontSize: fonts.sizes[1],
+    fontWeight: theme.typography.fontWeightMedium
   }
-})
+}))
 AppBar.displayName = "AppBarr"
 
 export default AppBar as React.FC<AppBarProps>
