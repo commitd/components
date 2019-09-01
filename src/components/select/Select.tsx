667dc20764677fdde71329321dd73b4660139103
Stuart Hendren <stuart@committed.io>
2019-09-01 13:58:02 +0100

    :sparkles: Add select component

---
+++ src/components/select/Select.tsx
@@ -0,0 +1,23 @@
+import * as React from "react"
+import {
+  compose,
+  spacing,
+  flexbox,
+  SpacingProps,
+  FlexboxProps
+} from "@material-ui/system"
+import MaterialSelect, {
+  SelectProps as MaterialSelectProps
+} from "@material-ui/core/Select"
+import { styled } from "@material-ui/styles"
+
+export type SelectProps = MaterialSelectProps & SpacingProps & FlexboxProps
+
+export const Select = styled(MaterialSelect)(
+  compose(
+    spacing,
+    flexbox
+  )
+)
+
+export default Select as React.FC<SelectProps>
