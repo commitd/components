0a33c2511141c4d54cdc08f340a54f99fc82710e
Stuart Hendren <stuart@committed.io>
2019-09-16 12:00:00 +0100

    Update palette to expose width prop

--- src/util/palette.tsx
+++ src/util/palette.tsx
@@ -6,22 +6,25 @@ import { Flex, Box, Theme, Heading } from '../'
 export interface PaletteProps {
   name: string
   theme: Theme
+  width?: number
 }
 
-export const Palette = withTheme(({ name, theme }: PaletteProps) => (
-  <Box p={1} width={1 / 3}>
-    <Color
-      alignItems="center"
-      justifyContent="center"
-      height="75px"
-      color={theme.palette[name].main}
-      style={{ color: theme.palette[name].contrastText }}
-    >
-      <Heading.h5>{name}</Heading.h5>
-    </Color>
-    <Flex>
-      <Color placement="bottom" color={theme.palette[name].light} />
-      <Color placement="bottom" color={theme.palette[name].dark} />
-    </Flex>
-  </Box>
-))
+export const Palette = withTheme(
+  ({ name, theme, width = 1 / 3 }: PaletteProps) => (
+    <Box p={1} width={width}>
+      <Color
+        alignItems="center"
+        justifyContent="center"
+        height="75px"
+        color={theme.palette[name].main}
+        style={{ color: theme.palette[name].contrastText }}
+      >
+        <Heading.h5>{name}</Heading.h5>
+      </Color>
+      <Flex>
+        <Color placement="bottom" color={theme.palette[name].light} />
+        <Color placement="bottom" color={theme.palette[name].dark} />
+      </Flex>
+    </Box>
+  )
+)
