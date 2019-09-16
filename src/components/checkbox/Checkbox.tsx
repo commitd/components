0ea7f1feac7717cc8948faddd296a2e7f4cde1a5
Stuart Hendren <stuart@committed.io>
2019-09-16 16:31:06 +0100

    Improvements to checkbox
    to better support custom icons only dray tick background if
    no icon supplied
    
    Adds color examples

--- src/components/checkbox/Checkbox.tsx
+++ src/components/checkbox/Checkbox.tsx
@@ -6,34 +6,40 @@ import { Box } from '../box/Box'
 export type CheckboxProps = MaterialCheckboxProps
 
 export const Checkbox: React.FC<CheckboxProps> = (props: CheckboxProps) => {
-  let color = 'transparent'
-  if (
-    ((props.value && props.value !== 'false') || props.checked) &&
-    !props.disabled
-  ) {
-    if (props.color === 'primary') {
-      color = 'secondary.dark'
+  if (!props.icon && !props.checkedIcon) {
+    let color = 'transparent'
+    if (
+      ((props.value && props.value !== 'false') || props.checked) &&
+      !props.disabled
+    ) {
+      if (props.color === 'primary') {
+        color = 'secondary.dark'
+      }
+      if (props.color === 'secondary') {
+        color = 'primary.main'
+      }
     }
-    if (props.color === 'secondary') {
-      color = 'primary.main'
-    }
-  }
-  return (
-    <Box position="relative">
+    return (
+      <Box position="relative">
+        <MaterialCheckbox {...props} style={{ zIndex: 110, ...props.style }} />
+        <Box
+          width="100%"
+          height="100%"
+          padding="14px"
+          bgcolor={color}
+          position="absolute"
+          zIndex={100}
+          top={0}
+          left={0}
+          style={{ backgroundClip: 'content-box' }}
+        />
+      </Box>
+    )
+  } else {
+    return (
       <MaterialCheckbox {...props} style={{ zIndex: 110, ...props.style }} />
-      <Box
-        width="100%"
-        height="100%"
-        padding="14px"
-        bgcolor={color}
-        position="absolute"
-        zIndex={100}
-        top={0}
-        left={0}
-        style={{ backgroundClip: 'content-box' }}
-      />
-    </Box>
-  )
+    )
+  }
 }
 
 export default Checkbox
