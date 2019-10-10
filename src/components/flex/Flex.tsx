6a83eb60c79f8f2561916f158105706de8c33061
Stuart Hendren <stuart@committed.io>
2019-10-10 16:18:51 +0100

    :art: Improve overview
    Because:
    - will be cleaer and more usefull

--- src/components/flex/Flex.tsx
+++ src/components/flex/Flex.tsx
@@ -2,9 +2,23 @@ import * as React from 'react'
 import { BoxProps, Box } from '../box/Box'
 
 export type FlexProps = Omit<BoxProps, 'display'>
+export type RowProps = Omit<FlexProps, 'flexDirection'>
+export type ColumnProps = Omit<FlexProps, 'flexDirection'>
 
 export type FlexRef = HTMLDivElement
 
 export const Flex = React.forwardRef<FlexRef, FlexProps>(
   (props: FlexProps, ref) => <Box ref={ref} {...props} display="flex" />
+)
+
+export const Row = React.forwardRef<FlexRef, RowProps>(
+  (props: FlexProps, ref) => (
+    <Box ref={ref} {...props} display="flex" flexDirection="row" />
+  )
+)
+
+export const Column = React.forwardRef<FlexRef, ColumnProps>(
+  (props: FlexProps, ref) => (
+    <Box ref={ref} {...props} display="flex" flexDirection="column" />
+  )
 )
