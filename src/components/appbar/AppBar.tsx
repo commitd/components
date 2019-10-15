fbda82512531e20ef759b815bc39025e0f38dbba
Stuart Hendren <stuart@committed.io>
2019-10-15 15:40:08 +0100

    :art: Improves AppBar docs
    Because:
    - props were full of noise

--- src/components/appbar/AppBar.tsx
+++ src/components/appbar/AppBar.tsx
@@ -1,4 +1,4 @@
-import { ComponentType, FC } from 'react'
+import { ComponentType, FC, AriaAttributes, DOMAttributes } from 'react'
 import { styled } from '@material-ui/styles'
 import { fonts, Theme } from '../../theme'
 import MaterialAppBar, {
@@ -18,6 +18,9 @@ export const AppBar: ComponentType<AppBarProps> = styled(MaterialAppBar)(
 
 // For documentation only
 export type BaseAppBarProps = Pick<AppBarProps, 'position' | 'color'>
-export type RestAppBarProps = Omit<AppBarProps, keyof BaseAppBarProps>
+export type RestAppBarProps = Omit<
+  AppBarProps,
+  keyof (BaseAppBarProps & AriaAttributes & DOMAttributes<'div'>)
+>
 export const BaseAppBarDocs: FC<BaseAppBarProps> = () => null
 export const RestAppBarDocs: FC<RestAppBarProps> = () => null
