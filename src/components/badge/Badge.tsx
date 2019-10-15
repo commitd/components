84d62595fc5211b8bb0ee0d320b0a22451b2412a
Stuart Hendren <stuart@committed.io>
2019-10-15 17:11:31 +0100

    :memo: Improve Badge docs

--- src/components/badge/Badge.tsx
+++ src/components/badge/Badge.tsx
@@ -1,8 +1,49 @@
-import * as React from 'react'
-import MaterialBadge, {
-  BadgeProps as MaterialBadgeProps
-} from '@material-ui/core/Badge'
+import { FC, HTMLAttributes } from 'react'
+import Badge, { BadgeProps } from '@material-ui/core/Badge'
+export type BadgeProps = BadgeProps
+export { Badge }
 
-export type BadgeProps = MaterialBadgeProps
-
-export const Badge: React.FC<BadgeProps> = MaterialBadge
+// For documentation only
+export type BaseBadgeProps = Omit<
+  BadgeProps,
+  keyof Omit<HTMLAttributes<HTMLDivElement>, 'color'>
+> & {
+  /**
+   * @default { vertical: 'top', horizontal: 'right' }
+   */
+  anchorOrigin?: {
+    vertical: 'top' | 'bottom'
+    horizontal: 'left' | 'right'
+  }
+  /**
+   * @default rectangle
+   */
+  overlap?: 'rectangle' | 'circle'
+  /**
+   * @default default
+   */
+  color?: 'primary' | 'secondary' | 'default' | 'error'
+  /**
+   * @default span
+   */
+  component?: React.ElementType<React.HTMLAttributes<HTMLDivElement>>
+  /**
+   * @default false
+   */
+  invisible?: boolean
+  /**
+   * @default 99
+   */
+  max?: number
+  /**
+   * @default false
+   */
+  showZero?: boolean
+  /**
+   * @default standard
+   */
+  variant?: 'standard' | 'dot'
+}
+export type RestBadgeProps = Omit<BadgeProps, keyof BaseBadgeProps>
+export const BaseBadgeDocs: FC<BaseBadgeProps> = () => null
+export const RestBadgeDocs: FC<RestBadgeProps> = () => null
