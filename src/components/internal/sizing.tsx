9c9f7dc064fa0072609cfe5a44d79fd57f5bafa3
Stuart Hendren <stuart@committed.io>
2019-10-16 18:01:52 +0100

    Improve typing on withPoritioning and withSizing
    This allos the use of the raw string components for native html elements.

--- src/components/internal/sizing.tsx
+++ src/components/internal/sizing.tsx
@@ -5,5 +5,5 @@ import { styled } from '@material-ui/styles'
 export type SizingProps = MaterialSixingProps
 
 export const withSizing = <P extends object>(
-  WrappedComponent: React.ComponentType<P & SizingProps>
-) => styled(WrappedComponent)(sizing)
+  WrappedComponent: React.ElementType<P>
+) => styled(WrappedComponent)(sizing) as React.ComponentType<P & SizingProps>
