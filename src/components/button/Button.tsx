a279a9cf01c7ae5f3fa4fb8217299bf19b84c268
Stuart Hendren <stuart@committed.io>
2019-10-16 10:33:26 +0100

    :memo: Improve Button docs

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { FC, ComponentType } from 'react'
 import MaterialButton, {
   ButtonProps as MaterialButtonProps
 } from '@material-ui/core/Button'
@@ -6,9 +6,18 @@ import { withNoOpener, withPositioning, PositioningProps } from '../internal'
 
 export type ButtonProps = MaterialButtonProps & PositioningProps
 
-export const Button: React.ComponentType<ButtonProps> = withPositioning(
+export const Button: ComponentType<ButtonProps> = withPositioning(
   withNoOpener(MaterialButton)
 )
 Button.defaultProps = {
   variant: 'contained'
 }
+
+// For documentation only
+export type BaseButtonProps = Pick<
+  MaterialButtonProps,
+  'children' | 'color' | 'fullWidth' | 'href' | 'size' | 'variant'
+>
+export type RestButtonProps = Omit<MaterialButtonProps, keyof BaseButtonProps>
+export const BaseButtonDocs: FC<BaseButtonProps> = () => null
+export const RestButtonDocs: FC<RestButtonProps> = () => null
