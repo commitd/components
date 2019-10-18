25f25729ccc9440bbd6a0f6ac20c9dbe3150d058
Stuart Hendren <stuart@committed.io>
2019-10-18 16:29:01 +0100

    :memo: Improve Tooltip docs

--- src/components/tooltip/Tooltip.tsx
+++ src/components/tooltip/Tooltip.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { FC, HTMLAttributes } from 'react'
 import MaterialTooltip, {
   TooltipProps as MaterialTooltipProps
 } from '@material-ui/core/Tooltip'
@@ -102,3 +102,36 @@ export const Tooltip = (props: TooltipProps) => {
     />
   )
 }
+
+// For documentation only
+export type TooltipDocsProps = Omit<
+  TooltipProps,
+  keyof Omit<HTMLAttributes<HTMLDivElement>, 'title'>
+> & {
+  /**
+   * If true, the tooltip is shown.
+   */
+  open?: boolean
+  /**
+   * Tooltip placement.
+   * @default bottom
+   */
+  placement?:
+    | 'bottom-end'
+    | 'bottom-start'
+    | 'bottom'
+    | 'left-end'
+    | 'left-start'
+    | 'left'
+    | 'right-end'
+    | 'right-start'
+    | 'right'
+    | 'top-end'
+    | 'top-start'
+    | 'top'
+  /**
+   * Tooltip title. Zero-length titles string are never displayed.
+   */
+  title: React.ReactNode
+}
+export const TooltipDocs: FC<TooltipDocsProps> = () => null
