8a0b7cf7f04d0750df91395b968c20a8aab70147
Stuart Hendren <stuart@committed.io>
2019-10-18 16:30:21 +0100

    Improve Typography
    Moves Text styling to Typography so can be used mroe easily

--- src/components/typography/Text.tsx
+++ src/components/typography/Text.tsx
@@ -1,50 +1,10 @@
-import React from 'react'
+import { ComponentType } from 'react'
 import { styled } from '@material-ui/styles'
 import { Typography, TypographyProps } from './Typography'
 import { fonts } from '../../theme'
 
-export type MainTextProps = {
-  /** Select from a scaled font size, 0 in normal, negative index for smaller, positive for larger */
-  fontSize?: number
-  /** Set the text alignment */
-  align?: 'left' | 'center' | 'right'
-  /** Set true to set title case */
-  capital?: boolean
-  /** Set true to set all upper */
-  upper?: boolean
-  /** Set true to use bold font */
-  bold?: boolean
-  /** Set true to use light font */
-  light?: boolean
-  /** Set true to use italic font  */
-  italic?: boolean
-}
-export type TextProps = TypographyProps & MainTextProps
-export type SpanProps = Omit<TextProps, 'component'>
-export type ParagraphProps = Omit<TextProps, 'component'>
+export type TextProps = TypographyProps
 
-export const Text: React.ComponentType<TextProps> = styled<
-  React.ComponentType<TextProps>
->(({ italic, fontSize, upper, light, capital, bold, ...others }: TextProps) => (
-  <Typography {...others} />
-))({
-  fontFamily: fonts.families.main,
-  fontStyle: props => (props.italic ? 'italic' : 'normal'),
-  fontSize: props => fonts.sizes[props.fontSize ? props.fontSize : 0],
-  // letterSpacing: props =>
-  //   props.upper
-  //     ? themeGet("letterSpacings.upper")(props)
-  //     : props.capital
-  //     ? themeGet("letterSpacings.title")(props)
-  //     : themeGet("letterSpacings.normal")(props),
-  textTransform: props =>
-    props.upper ? 'uppercase' : props.capital ? 'capitalize' : 'none',
-  fontWeight: props => (props.light ? 100 : props.bold ? 500 : 300)
+export const Text: ComponentType<TextProps> = styled(Typography)({
+  fontFamily: fonts.families.main
 })
-
-export const Span: React.FC<SpanProps> = props => (
-  <Text {...props} component="span" />
-)
-export const Paragraph: React.FC<ParagraphProps> = props => (
-  <Text {...props} component="p" />
-)
--- src/components/typography/Typography.tsx
+++ src/components/typography/Typography.tsx
@@ -1,25 +1,73 @@
-import * as React from 'react'
-import {
-  compose,
-  spacing,
-  flexbox,
-  SpacingProps,
-  FlexboxProps
-} from '@material-ui/system'
+import React, { ComponentType, FC, HTMLAttributes } from 'react'
 import { styled } from '@material-ui/styles'
 import MaterialTypography, {
   TypographyProps as MaterialTypographyProps
 } from '@material-ui/core/Typography'
+import { withPositioning, PositioningProps } from '../internal'
+import { fonts } from '../../theme'
 
-export type TypographyProps = MaterialTypographyProps &
-  SpacingProps &
-  FlexboxProps
-
-export const Typography: React.ComponentType<TypographyProps> = styled<
-  React.ComponentType<TypographyProps>
->(MaterialTypography)(
-  compose(
-    spacing,
-    flexbox
-  )
+type BaseTypographyProps = MaterialTypographyProps & PositioningProps
+const BaseTypography: ComponentType<BaseTypographyProps> = withPositioning(
+  MaterialTypography
 )
+
+export type ExtraTypographyProps = {
+  /** Select from a scaled font size, 0 in normal, negative index for smaller, positive for larger */
+  fontSize?: number
+  /** Set the text alignment */
+  align?: 'left' | 'center' | 'right'
+  /** Set true to set title case */
+  capital?: boolean
+  /** Set true to set all upper */
+  upper?: boolean
+  /** Set true to use bold font */
+  bold?: boolean
+  /** Set true to use light font */
+  light?: boolean
+  /** Set true to use italic font  */
+  italic?: boolean
+}
+
+export type TypographyProps = BaseTypographyProps & ExtraTypographyProps
+export type SpanProps = Omit<TypographyProps, 'component'>
+export type ParagraphProps = Omit<TypographyProps, 'component'>
+
+export const StyledTypography: ComponentType<TypographyProps> = styled(
+  ({
+    italic,
+    fontSize,
+    upper,
+    light,
+    capital,
+    bold,
+    ...others
+  }: TypographyProps) => <BaseTypography {...others} />
+)({
+  fontStyle: props => (props.italic ? 'italic' : 'normal'),
+  fontSize: props => fonts.sizes[props.fontSize ? props.fontSize : 0],
+  textTransform: props =>
+    props.upper ? 'uppercase' : props.capital ? 'capitalize' : 'none',
+  fontWeight: props => (props.light ? 100 : props.bold ? 500 : 300)
+})
+
+export const Typography: FC<TypographyProps> = (props: TypographyProps) => {
+  if (props.variant) {
+    return <BaseTypography {...props} />
+  } else {
+    return <StyledTypography {...props} />
+  }
+}
+
+export const Span: FC<SpanProps> = props => (
+  <Typography {...props} component="span" />
+)
+export const Paragraph: FC<ParagraphProps> = props => (
+  <Typography {...props} component="p" />
+)
+
+// For documentation only
+export type TypographyDocsProps = Omit<
+  ExtraTypographyProps & MaterialTypographyProps,
+  keyof Omit<HTMLAttributes<HTMLElement>, 'color'> | 'variantMapping'
+>
+export const TypographyDocs: FC<TypographyDocsProps> = () => null
