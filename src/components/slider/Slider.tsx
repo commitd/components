5ee44dd5b9e2aa38768f3d8a3d39866948e222ff
Stuart Hendren <stuart@committed.io>
2019-10-18 16:28:11 +0100

    :memo: Improve Slider docs

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { FC, HTMLAttributes } from 'react'
 import { withWrapper, WithWrapperProps } from '../internal'
 import MaterialSlider, {
   SliderProps as MaterialSliderProps
@@ -9,3 +9,10 @@ export type SliderProps = MaterialSliderProps & WithWrapperProps
 export const Slider: React.ComponentType<SliderProps> = withWrapper(
   MaterialSlider
 )
+
+// For documentation only
+export type SliderDocsProps = Omit<
+  MaterialSliderProps,
+  keyof Omit<HTMLAttributes<HTMLSpanElement>, 'color' | 'onChange' | 'track'>
+>
+export const SliderDocs: FC<SliderDocsProps> = () => null
