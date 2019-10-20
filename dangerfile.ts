95836be8f77a8e713431981ce524a134b31e3a3e
Stuart Hendren <stuart@committed.io>
2019-10-20 13:20:47 +0100

    Split out local dangerfile operations
    Adds checks for components missing stories

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,5 +1,4 @@
 import { danger, warn } from 'danger'
-import { includes } from 'lodash'
 import spellcheck from 'danger-plugin-spellcheck'
 
 // No PR is too small to include a description of why you made a change
@@ -14,32 +13,7 @@ if (danger.github.pr.assignee === null) {
   )
 }
 
-const modifiedFiles = danger.git.modified_files
-const newFiles = danger.git.created_files
-const allFiles = [...modifiedFiles, ...newFiles]
-
-// Keep lockfile up to date
-const packageChanged = includes(modifiedFiles, 'package.json')
-const lockfileChanged = includes(modifiedFiles, 'yarn.lock')
-if (packageChanged && !lockfileChanged) {
-  const message = 'Changes were made to package.json, but not to yarn.lock'
-  const idea = 'Perhaps you need to run `yarn install` or commit the lockfile?'
-  warn(`${message} - <i>${idea}</i>`)
-}
-if (!packageChanged && lockfileChanged) {
-  const message = 'Changes were made to yarn.lock, but not to package.json'
-  const idea = 'Perhaps you need to commit the package.json?'
-  warn(`${message} - <i>${idea}</i>`)
-}
-
-// Request changes to src also include changes to tests.
-const hasSrcChanges = allFiles.some(p => includes(p, '.tsx'))
-const hasStoriesChanges = allFiles.some(p => includes(p, '.stories.'))
-
-if (hasSrcChanges && !hasStoriesChanges) {
-  warn(
-    'This PR does not include changes to stories, even though it affects app code.'
-  )
-}
-
 spellcheck({ settings: 'spellcheck.json' })
+
+// Re-run the danger lite
+import './dangerfile.lite'
