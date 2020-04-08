fcd96cb479a67709b50ffadedd88342c98825be3
Stuart Hendren <stuart@committed.io>
2020-04-08 17:51:20 +0100

    Add png typing

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -17,6 +17,11 @@ declare module '*.svg' {
   export { svgComponent as ReactComponent }
 }
 
+declare module '*.png' {
+  const value: any
+  export default value
+}
+
 declare module '*.woff'
 declare module '*.woff2'
 declare module '*.ttf'
