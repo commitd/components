4e630a7bfa11b9615415fa70d0d3c881291d0f2f
Stuart Hendren <stuart@committed.io>
2020-05-05 11:37:38 +0100

    Improve typing of exported makeStyles
    Exports overloaded types defined by material,
    one does not require props in use styles.
    
    fixes #44

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,11 +1,17 @@
 // Re exporting
 export { useWidth } from './useWidth'
 export { createStyles, useTheme } from '@material-ui/styles'
-
 import { Theme as CommittedTheme } from '../theme'
-import { Styles, WithStylesOptions } from '@material-ui/styles/withStyles'
+import {
+  Styles,
+  WithStylesOptions,
+  ClassNameMap,
+} from '@material-ui/styles/withStyles'
 import { makeStyles as MaterialMakeStyles } from '@material-ui/styles'
 
+/**
+ * `makeStyles` where the passed `styles` do depend on props
+ */
 export function makeStyles<
   Theme = CommittedTheme,
   Props extends {} = {},
@@ -13,6 +19,17 @@ export function makeStyles<
 >(
   styles: Styles<Theme, Props, ClassKey>,
   options?: Omit<WithStylesOptions<Theme>, 'withTheme'>
-) {
-  return MaterialMakeStyles<Theme, Props, ClassKey>(styles, options)
+): (props: Props) => ClassNameMap<ClassKey>
+
+/**
+ * `makeStyles` where the passed `styles` do not depend on props
+ */
+export function makeStyles<
+  Theme = CommittedTheme,
+  ClassKey extends string = string
+>(
+  style: Styles<Theme, {}, ClassKey>,
+  options?: Omit<WithStylesOptions<Theme>, 'withTheme'>
+): (props?: any) => ClassNameMap<ClassKey> {
+  return MaterialMakeStyles<Theme, {}, ClassKey>(style, options)
 }
