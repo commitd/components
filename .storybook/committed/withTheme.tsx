8108c5b870a7285e8ac91042c13f523de61b5dbc
Stuart Hendren <stuart@committed.io>
2020-12-15 14:28:20 +0000

    Updating dependencies
    Adds temp background to stories

--- .storybook/committed/withTheme.tsx
+++ .storybook/committed/withTheme.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useDarkMode } from 'storybook-dark-mode'
-import { ThemeProvider } from '../../src'
+import { ThemeProvider, Box } from '../../src'
 
 /**
  * Wrap a component with the default ThemeProvider
@@ -11,7 +11,10 @@ export const withTheme = (Story) => {
   const choice = useDarkMode() ? 'dark' : 'light'
   return (
     <ThemeProvider choice={choice}>
-      <Story />
+      {/* Temporary background until themeing in docs is supported */}
+      <Box bgcolor="background.paper" p={3}>
+        <Story />
+      </Box>
     </ThemeProvider>
   )
 }
