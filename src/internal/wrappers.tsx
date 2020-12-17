8a1c0089dd2b5b55d33387fe470640180535a230
Stuart Hendren <stuart@committed.io>
2020-12-17 16:03:43 +0000

    Remove legacy withPositioning

--- src/internal/wrappers.tsx
+++ src/internal/wrappers.tsx
@@ -23,14 +23,6 @@ export type PositioningProps = SpacingProps &
 
 export type BoxProps = PositioningProps & SizingProps
 
-// The natural typing causes typescript to error, hence the casts to force it.
-export const withPositioning = <P extends object>(
-  WrappedComponent: React.ElementType<P>
-) =>
-  (styled(WrappedComponent)(
-    compose(display, spacing, flexbox, griditem)
-  ) as unknown) as React.ComponentType<P & PositioningProps>
-
 export const sizingPropsKeys: Array<keyof SizingProps> = [
   'width',
   'maxWidth',
