2565f336490fb3fdd0d9944650395b9ade57b85c
Stuart Hendren <stuart@committed.io>
2021-05-19 11:01:17 +0000

    Adding Text components
    See #81

--- src/components/Text/Text.stories.tsx
+++ src/components/Text/Text.stories.tsx
@@ -1,10 +1,11 @@
 import React, { ComponentProps } from 'react'
 import { Story, Meta } from '@storybook/react'
-import { Text } from '.'
+import { Text, Paragraph, Monospace, Caption } from '.'
 import { Column, Row } from '../'
 export default {
   title: 'Components/Text',
   component: Text,
+  subcomponents: { Paragraph, Monospace, Caption },
   argTypes: {
     size: {
       control: { type: 'range', min: -2, max: 8, step: 1 },
@@ -59,6 +60,10 @@ export const Size = () => (
   </Column>
 )
 
+/**
+ * Light, regular and bold can be set with the weight prop. The fonts are supplied in these weights.
+ * Other weights can be set through the css if required.
+ */
 export const Weight = () => (
   <Row css={{ justifyContent: 'space-between' }}>
     <Text weight="light">Light text</Text>
@@ -67,6 +72,9 @@ export const Weight = () => (
   </Row>
 )
 
+/**
+ * A set of fonts for most use cases is supplied.
+ */
 export const Fonts = () => (
   <Column>
     <Text font="default">Default font is Inter</Text>
@@ -85,6 +93,9 @@ export const Fonts = () => (
   </Column>
 )
 
+/**
+ * Demonstration of how different styles can be achieved.
+ */
 export const Variations = () => (
   <Row
     css={{
@@ -106,3 +117,137 @@ export const Variations = () => (
     </Text>
   </Row>
 )
+
+/**
+ * Text components use span by default, so they can be nested to supply inline styles.
+ */
+export const Inline = () => (
+  <Row
+    css={{
+      justifyContent: 'space-around',
+      flexWrap: 'wrap',
+      alignItems: 'baseline',
+      '& span': {
+        whiteSpace: 'nowrap',
+      },
+    }}
+  >
+    <Text>
+      This is <Text weight="light">Light</Text> text
+    </Text>
+    <Text>
+      This is <Text weight="bold">Bold</Text> text
+    </Text>
+    <Text>
+      This is <Text css={{ textTransform: 'uppercase' }}>uppercase</Text> text
+    </Text>
+    <Text>
+      This is <Text css={{ textTransform: 'capitals' }}>capitals</Text> text
+    </Text>
+    <Text>
+      This is <Text italic>Italic</Text> text
+    </Text>
+    <Text>
+      This is <Text font="monospace">monospaced</Text> text
+    </Text>
+    <Text>
+      This is <Text color="primary">Primary</Text> text
+    </Text>
+    <Text>
+      This is <Text color="secondary">Secondary</Text> text
+    </Text>
+    <Text>
+      This is <Text as="s">strikethrough</Text> text
+    </Text>
+    <Text>
+      This is{' '}
+      <Text as="s" weight="bold" italic>
+        multiple
+      </Text>{' '}
+      text
+    </Text>
+  </Row>
+)
+
+/**
+ * A paragraph component is supplied to use the `<p>` tag.
+ */
+export const ParagraphStory: Story = () => (
+  <>
+    <Paragraph>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam bibendum,
+      nisl a fermentum viverra, diam urna efficitur ante, sit amet condimentum
+      dolor tellus in augue. Vivamus eu nulla risus. Lorem ipsum dolor sit amet,
+      consectetur adipiscing elit. Quisque nec purus mi. Morbi pretium massa
+      sem, ac semper elit dictum in. Suspendisse hendrerit lorem quis lobortis
+      pellentesque. Ut blandit efficitur urna eu fermentum. Phasellus venenatis
+      pretium quam, eget elementum enim congue non. Nunc vitae quam semper,
+      mollis augue ac, facilisis sem. Sed justo urna, accumsan nec congue eu,
+      hendrerit vel purus. Maecenas ultricies luctus nisi a pharetra.
+    </Paragraph>
+    <Paragraph>
+      Aenean nec pretium turpis, et vulputate libero. Sed pharetra risus elit.
+      Vivamus quis facilisis felis, at mollis tortor. Duis enim massa, lobortis
+      eget semper sit amet, tristique nec lacus. Ut malesuada odio consectetur
+      lobortis iaculis. Vestibulum elementum, turpis et venenatis aliquam, urna
+      neque iaculis erat, a pellentesque ex nibh eu mi. Quisque id vehicula
+      ipsum. Ut lacinia egestas congue. Curabitur laoreet tortor sapien, vitae
+      ornare sem porttitor sit amet. Cras congue lorem ullamcorper, venenatis mi
+      id, suscipit nisl.
+    </Paragraph>
+    <Paragraph>
+      Suspendisse gravida sed ipsum a malesuada. Nulla facilisi. Suspendisse
+      mollis purus eget risus mattis facilisis. Pellentesque eu est efficitur,
+      porttitor justo vel, mollis felis. Morbi et tortor nulla. Nullam mattis
+      quis arcu nec dignissim. Nam malesuada, lorem id gravida ultricies, odio
+      libero blandit sem, quis dictum mi neque eget justo. Nulla ultrices, nunc
+      sit amet luctus facilisis, mi augue viverra metus, ac eleifend risus nibh
+      nec est. Maecenas sit amet rhoncus turpis. Sed ac arcu id diam tincidunt
+      porta pulvinar vel enim.
+    </Paragraph>
+    <Paragraph>
+      Phasellus quis hendrerit nisl, sit amet hendrerit est. Sed vitae eleifend
+      augue. Donec imperdiet, magna sit amet tempus tristique, ante mi rhoncus
+      eros, nec porttitor erat turpis eget justo. Nulla non aliquam tellus, sed
+      hendrerit urna. Aliquam purus arcu, tempor nec lacinia ac, vulputate et
+      nibh. Praesent vitae leo lacus. Mauris elementum vitae eros id congue.
+      Integer dapibus bibendum diam, a aliquet nunc. Aliquam erat volutpat. Duis
+      aliquam vel nunc vitae pulvinar. Cras justo ipsum, suscipit ac imperdiet
+      hendrerit, pharetra sit amet est. Quisque nec nisi erat. Interdum et
+      malesuada fames ac ante ipsum primis in faucibus. Aliquam iaculis rhoncus
+      condimentum.
+    </Paragraph>
+    <Paragraph>
+      Integer semper massa vitae lorem ullamcorper faucibus. Suspendisse augue
+      diam, viverra id molestie quis, semper vel velit. Vivamus ac porttitor
+      nibh, ut sollicitudin neque. Cras viverra pretium eros ac condimentum. Sed
+      eget nunc faucibus, luctus justo id, commodo erat. Nulla vestibulum
+      hendrerit tellus at tincidunt. Suspendisse erat felis, venenatis sed
+      turpis id, efficitur elementum libero. Integer convallis in est vel
+      euismod. Curabitur vitae orci vitae justo venenatis rhoncus. Donec eget
+      lorem efficitur, posuere velit quis, egestas nisi. Vivamus non lacus a
+      turpis blandit sodales vitae nec lacus.
+    </Paragraph>
+  </>
+)
+ParagraphStory.parameters = {
+  title: 'Paragraph',
+}
+
+/**
+ * Caption component uses a smaller font and the tag can still be configured using `as` so it can be set to `caption`, `figcaption` etc. as required.
+ */
+export const CaptionStory = () => <Caption>To be used for captions</Caption>
+CaptionStory.parameters = {
+  title: 'Caption',
+}
+
+/**
+ * Monospace component uses the monospace font and `pre` tag
+ */
+export const MonospaceStory = () => (
+  <Monospace>To be used for pre components, uses the monospace font.</Monospace>
+)
+MonospaceStory.parameters = {
+  title: 'Monospace',
+}
--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -99,10 +99,43 @@ type TextComponent = Polymorphic.ForwardRefComponent<
 >
 
 /**
- * Text component
+ * Text component covers all text use in the components.
+ *
+ * Prefer styling using the supplied props options, but the standard css is also available if required.
+ *
+ * A set of pre-configured components are also supplied for common use cases. These all use the same underlying Text component.
  */
 export const Text = forwardRef((props, forwardedRef) => {
   return <StyledText {...props} ref={forwardedRef} />
 }) as TextComponent
 
 Text.toString = () => `.${StyledText.className}`
+
+export const Paragraph = forwardRef<HTMLParagraphElement, TextOwnProps>(
+  ({ css, ...props }, forwardedRef) => {
+    return (
+      <Text css={{ mb: '$3', ...css }} as="p" {...props} ref={forwardedRef} />
+    )
+  }
+)
+
+/**
+ * Monospace component uses the monospace font and `pre` tag
+ */
+export const Monospace = forwardRef<HTMLPreElement, TextOwnProps>(
+  (props, forwardedRef) => {
+    return <Text as="pre" font="monospace" {...props} ref={forwardedRef} />
+  }
+)
+
+type CaptionComponent = Polymorphic.ForwardRefComponent<
+  typeof DEFAULT_TAG,
+  TextOwnProps
+>
+
+/**
+ * Captions styles the text for use in a caption. This remain polymorphic, so the component type can be set to `caption` or `figcaption` if required.
+ */
+export const Caption = forwardRef((props, forwardedRef) => {
+  return <Text size={-2} {...props} ref={forwardedRef} />
+}) as CaptionComponent
