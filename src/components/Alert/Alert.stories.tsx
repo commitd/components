e5ad417bff849caf5fb29f25e4bd91df6c38016d
Stuart Hendren <stuart@committed.io>
2021-05-20 17:08:54 +0000

    Adds Alert components
    See #81

---
+++ src/components/Alert/Alert.stories.tsx
@@ -0,0 +1,88 @@
+import React from 'react'
+import { Story, Meta } from '@storybook/react'
+import { Alert, AlertTitle, AlertContent } from '.'
+import { Column } from '../'
+
+export default {
+  title: 'Components/Alert',
+  component: Alert,
+  subcomponents: { AlertTitle, AlertContent },
+  argTypes: {
+    severity: {
+      control: {
+        type: 'select',
+        options: ['error', 'warning', 'info', 'success', 'ghost'],
+      },
+      defaultValue: 'error',
+      description: 'Set the severity of the alert',
+    },
+  },
+} as Meta
+
+const Template: Story = (args) => (
+  <Alert {...args}>
+    <AlertTitle>Alert Title</AlertTitle>
+    <AlertContent>
+      This is the alert content, the content is optional but should usually give
+      further explanation about the alert and if any action is required.
+    </AlertContent>
+  </Alert>
+)
+export const Default = Template.bind({
+  severity: 'error',
+})
+
+export const Severity = () => (
+  <Column css={{ gap: '$3' }}>
+    <Alert severity="error">
+      <AlertTitle>This is an error alert</AlertTitle>
+    </Alert>
+    <Alert severity="warning">
+      <AlertTitle>This is a warning alert</AlertTitle>
+    </Alert>
+    <Alert severity="info">
+      <AlertTitle>This is an info alert</AlertTitle>
+    </Alert>
+    <Alert severity="success">
+      <AlertTitle>This is a success alert</AlertTitle>
+    </Alert>
+    <Alert severity="ghost">
+      <AlertTitle>This is a ghost alert</AlertTitle>
+    </Alert>
+  </Column>
+)
+
+export const WithContent = () => (
+  <Column css={{ gap: '$3' }}>
+    <Alert severity="ghost">
+      <AlertTitle>Ghost</AlertTitle>
+      <AlertContent>
+        This is a ghost alert — <strong>check it out!</strong>
+      </AlertContent>
+    </Alert>
+    <Alert severity="error">
+      <AlertTitle>Error</AlertTitle>
+      <AlertContent>
+        This is an error alert — <strong>check it out!</strong>
+      </AlertContent>
+    </Alert>
+    <Alert severity="warning">
+      <AlertTitle>Warning</AlertTitle>
+      <AlertContent>
+        This is a warning alert — <strong>check it out!</strong>
+      </AlertContent>
+    </Alert>
+    <Alert severity="info">
+      <AlertTitle>Info</AlertTitle>
+      <AlertContent>
+        This is an info alert — <strong>check it out!</strong>
+      </AlertContent>
+    </Alert>
+    <Alert severity="success">
+      <AlertTitle>Success</AlertTitle>
+      <AlertContent>
+        This is a success alert — <strong>check it out!</strong>
+      </AlertContent>
+    </Alert>
+  </Column>
+)
---
+++ src/components/Alert/Alert.tsx
@@ -0,0 +1,83 @@
+import type * as Polymorphic from '@radix-ui/react-polymorphic'
+import React, { FC, forwardRef } from 'react'
+import { CSS, StitchesVariants, styled } from 'stitches.config'
+import { Heading, Text } from '../'
+
+const DEFAULT_TAG = 'div'
+
+const StyledAlert = styled(DEFAULT_TAG, {
+  // Reset
+  boxSizing: 'border-box',
+  '&::before': {
+    boxSizing: 'border-box',
+  },
+  '&::after': {
+    boxSizing: 'border-box',
+  },
+
+  border: '1px solid',
+  borderRadius: '$2',
+
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '$2',
+
+  p: '$4',
+
+  variants: {
+    severity: {
+      ghost: {
+        backgroundColor: '$paper',
+        borderColor: '$grey500',
+        color: '$text',
+      },
+      warning: {
+        backgroundColor: '$warningBackground',
+        borderColor: '$warningHighlight',
+        color: '$warningHighlight',
+      },
+      info: {
+        backgroundColor: '$infoBackground',
+        borderColor: '$infoHighlight',
+        color: '$infoHighlight',
+      },
+      success: {
+        backgroundColor: '$successBackground',
+        borderColor: '$successHighlight',
+        color: '$successHighlight',
+      },
+      error: {
+        backgroundColor: '$errorBackground',
+        borderColor: '$errorHighlight',
+        color: '$errorHighlight',
+      },
+    },
+  },
+})
+
+export const AlertTitle: FC = ({ children }) => (
+  <Heading as="div" size={1} css={{ color: 'inherit' }} variant="h6">
+    {children}
+  </Heading>
+)
+
+export const AlertContent: FC = ({ children }) => (
+  <Text css={{ color: 'inherit' }} size={-1}>
+    {children}
+  </Text>
+)
+
+type AlertCSSProp = { css?: CSS }
+type AlertVariants = StitchesVariants<typeof StyledAlert>
+type AlertOwnProps = AlertCSSProp & AlertVariants
+
+type AlertComponent = Polymorphic.ForwardRefComponent<
+  typeof DEFAULT_TAG,
+  AlertOwnProps
+>
+
+export const Alert = forwardRef((props, forwardedRef) => {
+  return <StyledAlert {...props} ref={forwardedRef} />
+}) as AlertComponent
+
+Alert.toString = () => `.${StyledAlert.className}`
