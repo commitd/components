4f8559678736ba4d81188fd0a81b7137f1d73481
Stuart Hendren <stuart@committed.io>
2021-06-25 13:45:38 +0000

    Updates storybook to 6.3
    This has some nice new features and may allow us to update typescript.
    
    Removes the portalled utility as was fixed by earlier changes to the theme
    decoarator so no longer needed.
    
    Left in a story incase anyone hits issues with it.
    
    fixes #165

--- src/docs/util/usePortalled.ts
+++
@@ -1,8 +0,0 @@
-/**
- *
- * Gets the portalled state from the  story context and parsed as boolean form string.
- * See .storybook/preview.js for globals setup and https://storybook.js.org/docs/react/essentials/toolbars-and-globals for documentation.
- */
-export const usePortalled = (context: { globals: any }) => {
-  return JSON.parse(context?.globals?.portalled || 'true') as boolean
-}
