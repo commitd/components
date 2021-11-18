31f61a892e4e0621f4aa87ccc9d1d533678701ae
Stuart Hendren <stuart@committed.io>
2021-11-18 18:03:04 +0000

    feat(hidden): adds Hidden component
    This is for use when we want to visually hide an element but preserve accessibility
    
    fix #233

---
+++ src/components/Hidden/Hidden.tsx
@@ -0,0 +1,34 @@
+import React, { ElementRef, ComponentPropsWithoutRef, forwardRef } from 'react'
+import { useBoolean, useKeyboard } from '@committed/hooks'
+import { Root } from '@radix-ui/react-visually-hidden'
+
+/**
+ * Hidden component
+ *
+ * Visually hides the component while preserving the content in a screen accessible way.
+ *
+ * Based on [Radix VisuallyHidden](https://www.radix-ui.com/docs/primitives/utilities/visually-hidden).
+ *
+ * Has additional development mode function to show the hidden element when `alt` is pressed.
+ */
+export const Hidden = forwardRef<
+  ElementRef<typeof Root>,
+  ComponentPropsWithoutRef<typeof Root>
+>(({ children, ...props }, forwardedRef) => {
+  const [forceShow, { setTrue, setFalse }] = useBoolean(false)
+  useKeyboard('', setFalse, { event: 'keyup', development: true })
+  useKeyboard('alt', setTrue, {
+    event: 'keydown',
+    ignoreRepeat: true,
+    development: true,
+  })
+
+  if (forceShow) {
+    return <span>{children}</span>
+  }
+  return (
+    <Root {...props} ref={forwardedRef}>
+      {children}
+    </Root>
+  )
+})
