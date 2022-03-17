26b696857e1f4d76a8ba20885b761bbfcc2b50b7
Kristian Aspinall <kristian@committed.io>
2022-03-17 16:53:47 +0000

    feat: began creating toast component based on radix

---
+++ src/components/Toast/Toast.stories.tsx
@@ -0,0 +1,96 @@
+import React from 'react'
+import { Story, Meta } from '@storybook/react'
+import {
+  Toast,
+  ToastAction,
+  ToastDescription,
+  ToastProvider,
+  ToastTitle,
+  ToastViewport,
+} from '.'
+import { Button } from '../Button'
+
+export default {
+  title: 'Components/Toast',
+  component: Toast,
+  subcomponents: {
+    Toast,
+    ToastAction,
+    ToastDescription,
+    ToastProvider,
+    ToastTitle,
+    ToastViewport,
+  },
+  argTypes: {
+    severity: {
+      control: {
+        type: 'select',
+        options: ['error', 'warning', 'info', 'success', 'ghost'],
+      },
+      defaultValue: 'error',
+      description: 'Set the severity of the alert',
+    },
+  },
+} as Meta
+
+export const Default: Story = () => {
+  const [open, setOpen] = React.useState(false)
+  const eventDateRef = React.useRef(new Date())
+  const timerRef = React.useRef(0)
+
+  React.useEffect(() => {
+    return () => clearTimeout(timerRef.current)
+  }, [])
+
+  function oneWeekAway() {
+    const now = new Date()
+    const inOneWeek = now.setDate(now.getDate() + 7)
+    return new Date(inOneWeek)
+  }
+
+  return (
+    <ToastProvider swipeDirection="right">
+      <Button
+        onClick={() => {
+          setOpen(false)
+          window.clearTimeout(timerRef.current)
+          timerRef.current = window.setTimeout(() => {
+            eventDateRef.current = oneWeekAway()
+            setOpen(true)
+          }, 100)
+        }}
+      >
+        Add to calendar
+      </Button>
+
+      <Toast open={open} onOpenChange={setOpen}>
+        <ToastTitle>Scheduled: Catch up</ToastTitle>
+        <ToastDescription asChild>
+          <time dateTime={eventDateRef.current.toISOString()}>
+            {eventDateRef.current.toLocaleString()}
+          </time>
+        </ToastDescription>
+        <ToastAction asChild altText="Goto schedule to undo">
+          <Button variant="primary" size="small">
+            Undo
+          </Button>
+        </ToastAction>
+      </Toast>
+      <ToastViewport />
+    </ToastProvider>
+  )
+}
+
+export const Open = () => (
+  <ToastProvider swipeDirection="right">
+    <Toast open={true}>
+      <ToastTitle>Scheduled: Catch up</ToastTitle>
+      <ToastDescription asChild>Text goes here</ToastDescription>
+      <ToastAction asChild altText="Goto schedule to undo">
+        <Button variant="primary" size="small">
+          Undo
+        </Button>
+      </ToastAction>
+    </Toast>
+  </ToastProvider>
+)
