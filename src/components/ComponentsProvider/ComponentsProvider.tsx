05bd4f16687c67fa35650fc00ebdc90a3fb61d9e
Stuart Hendren <stuart@committed.io>
2022-05-17 11:18:18 +0000

    fix(componentsprovider): explicitly add children prop
    This is to remove issue with react 18 asfter it removed children from FC
    
    fix #275

--- src/components/ComponentsProvider/ComponentsProvider.tsx
+++ src/components/ComponentsProvider/ComponentsProvider.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import React, { FC, PropsWithChildren } from 'react'
 import { ConditionalWrapper } from '../../utils'
 import { ThemeProvider, ThemeProviderProps } from '../ThemeProvider'
 import { ToastProvider, ToastViewport } from '../Toast'
@@ -51,13 +51,9 @@ export type ComponentsProviderProps = {
  * For each provider/component a configuration object can be passed with the relevant props.
  * To disable a particular provider set the configuration parameter to `false`.
  */
-export const ComponentsProvider: FC<ComponentsProviderProps> = ({
-  theme = {},
-  tooltip = {},
-  toast = {},
-  viewport = {},
-  children,
-}) => (
+export const ComponentsProvider: FC<
+  PropsWithChildren<ComponentsProviderProps>
+> = ({ theme = {}, tooltip = {}, toast = {}, viewport = {}, children }) => (
   <ConditionalWrapper
     condition={toast}
     wrapper={(wrappedChildren) => (
