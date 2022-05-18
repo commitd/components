f51159b0e3d65a5d26e1a7a66a080032a0e23166
Stuart Hendren <stuart@committed.io>
2022-05-18 14:29:55 +0000

    feat(dialog): improves behaviour of dialogs and provides more option
    Adds the dialog portal by default and allow it to be turned off using a prop. Also allows the
    provision of a container. These have been extended to other dialog based components e.g. Drawer,
    ConfirmDialog.
    
    fix #274

--- src/components/Dialog/Dialog.stories.tsx
+++ src/components/Dialog/Dialog.stories.tsx
@@ -1,7 +1,7 @@
 import { useBoolean } from '@committed/hooks'
 import { action } from '@storybook/addon-actions'
 import { Meta, Story } from '@storybook/react'
-import React from 'react'
+import React, { useState } from 'react'
 import {
   Dialog,
   DialogClose,
@@ -10,7 +10,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from '.'
-import { Button, IconButton, Link, Row, Text } from '../'
+import { Box, Button, Column, IconButton, Link, Row, Text } from '../'
 import { Close as CloseIcon } from '../Icons'
 
 export default {
@@ -92,5 +92,256 @@ export const CloseButton: Story = () => {
         </DialogContent>
       </Dialog>
     </>
+  )
+}
+
+/**
+ * The dialog is portalled by default. This means the dialog will be appended to the body and so over lay other elements.
+ * In addition, the `CompontsProvider`, by default, isolates the children in their own stacking context so this will not be afected by the z-index of other elements of the UI.
+ *
+ * This example shows how the z-index of other elements in the UI do not affect the dialog.
+ *
+ * This may not be the desired behaviour for your application so these defaults can be overridden, see below.
+ */
+export const Portalled: Story = () => {
+  return (
+    <Row gap centered>
+      <Dialog>
+        <DialogTrigger>
+          <Button>Default</Button>
+        </DialogTrigger>
+        <DialogContent overlayCss={{ backgroundColor: '$paper', opacity: 0.9 }}>
+          This is a dialog
+        </DialogContent>
+      </Dialog>
+      <Box css={{ zIndex: 10, backgroundColor: '$success4' }}>
+        <Dialog>
+          <DialogTrigger>
+            <Button>zIndex: 10</Button>
+          </DialogTrigger>
+          <DialogContent
+            overlayCss={{ backgroundColor: '$success4', opacity: 0.9 }}
+          >
+            This is a dialog
+          </DialogContent>
+        </Dialog>
+      </Box>
+      <Box css={{ zIndex: 100, backgroundColor: '$info4' }}>
+        <Dialog>
+          <DialogTrigger>
+            <Button>zIndex: 100</Button>
+          </DialogTrigger>
+          <DialogContent
+            overlayCss={{ backgroundColor: '$info4', opacity: 0.9 }}
+          >
+            This is a dialog
+          </DialogContent>
+        </Dialog>
+      </Box>
+      <Box css={{ zIndex: 1000, backgroundColor: '$warning4' }}>
+        <Dialog>
+          <DialogTrigger>
+            <Button>zIndex: 1000</Button>
+          </DialogTrigger>
+          <DialogContent
+            overlayCss={{ backgroundColor: '$warning4', opacity: 0.9 }}
+          >
+            This is a dialog
+          </DialogContent>
+        </Dialog>
+      </Box>
+      <Box css={{ zIndex: 10000, backgroundColor: '$error4' }}>
+        <Dialog>
+          <DialogTrigger>
+            <Button>zIndex: 10000</Button>
+          </DialogTrigger>
+          <DialogContent
+            overlayCss={{ backgroundColor: '$error4', opacity: 0.9 }}
+          >
+            This is a dialog
+          </DialogContent>
+        </Dialog>
+      </Box>
+    </Row>
+  )
+}
+
+/**
+ * Turning off portalling will mean the zIndex of the dialog will be relative to the zIndex of the element it is appended to.
+ */
+export const zIndex: Story = () => {
+  return (
+    <Row gap centered>
+      <Dialog>
+        <DialogTrigger>
+          <Button>Default</Button>
+        </DialogTrigger>
+        <DialogContent
+          overlayCss={{ backgroundColor: '$paper', opacity: 0.9 }}
+          portalled={false}
+        >
+          This is a dialog
+        </DialogContent>
+      </Dialog>
+      <Box css={{ zIndex: 10, backgroundColor: '$success4' }}>
+        <Dialog>
+          <DialogTrigger>
+            <Button>zIndex: 10</Button>
+          </DialogTrigger>
+          <DialogContent
+            portalled={false}
+            overlayCss={{ backgroundColor: '$success4', opacity: 0.9 }}
+          >
+            This is a dialog
+          </DialogContent>
+        </Dialog>
+      </Box>
+      <Box css={{ zIndex: 100, backgroundColor: '$info4' }}>
+        <Dialog>
+          <DialogTrigger>
+            <Button>zIndex: 100</Button>
+          </DialogTrigger>
+          <DialogContent
+            portalled={false}
+            overlayCss={{ backgroundColor: '$info4', opacity: 0.9 }}
+          >
+            This is a dialog
+          </DialogContent>
+        </Dialog>
+      </Box>
+      <Box css={{ zIndex: 1000, backgroundColor: '$warning4' }}>
+        <Dialog>
+          <DialogTrigger>
+            <Button>zIndex: 1000</Button>
+          </DialogTrigger>
+          <DialogContent
+            portalled={false}
+            overlayCss={{ backgroundColor: '$warning4', opacity: 0.9 }}
+          >
+            This is a dialog
+          </DialogContent>
+        </Dialog>
+      </Box>
+      <Box css={{ zIndex: 10000, backgroundColor: '$error4' }}>
+        <Dialog>
+          <DialogTrigger>
+            <Button>zIndex: 10000</Button>
+          </DialogTrigger>
+          <DialogContent
+            portalled={false}
+            overlayCss={{ backgroundColor: '$error4', opacity: 0.9 }}
+          >
+            This is a dialog
+          </DialogContent>
+        </Dialog>
+      </Box>
+    </Row>
+  )
+}
+
+/**
+ * The element the dialog portalls to can be provided, and different effects can be achieved by supplying additional css. For example:
+ */
+export const Container: Story = () => {
+  const [element, setElement] = useState<HTMLElement | null>(null)
+
+  return (
+    <Column gap>
+      <Row gap centered>
+        <Dialog>
+          <DialogTrigger>
+            <Button>Default</Button>
+          </DialogTrigger>
+          <DialogContent
+            container={element}
+            overlayCss={{
+              position: 'absolute',
+            }}
+            css={{ position: 'absolute', marginTop: 0 }}
+          >
+            This is a dialog
+          </DialogContent>
+        </Dialog>
+        <Box css={{ zIndex: 10, backgroundColor: '$success4' }}>
+          <Dialog>
+            <DialogTrigger>
+              <Button>zIndex: 10</Button>
+            </DialogTrigger>
+            <DialogContent
+              container={element}
+              overlayCss={{
+                backgroundColor: '$success4',
+                opacity: 0.9,
+                position: 'absolute',
+              }}
+              css={{ position: 'absolute', marginTop: 0 }}
+            >
+              This is a dialog
+            </DialogContent>
+          </Dialog>
+        </Box>
+        <Box css={{ zIndex: 100, backgroundColor: '$info4' }}>
+          <Dialog>
+            <DialogTrigger>
+              <Button>zIndex: 100</Button>
+            </DialogTrigger>
+            <DialogContent
+              container={element}
+              overlayCss={{
+                backgroundColor: '$info4',
+                opacity: 0.9,
+                position: 'absolute',
+              }}
+              css={{ position: 'absolute', marginTop: 0 }}
+            >
+              This is a dialog
+            </DialogContent>
+          </Dialog>
+        </Box>
+        <Box css={{ zIndex: 1000, backgroundColor: '$warning4' }}>
+          <Dialog>
+            <DialogTrigger>
+              <Button>zIndex: 1000</Button>
+            </DialogTrigger>
+            <DialogContent
+              container={element}
+              overlayCss={{
+                backgroundColor: '$warning4',
+                opacity: 0.9,
+                position: 'absolute',
+              }}
+              css={{ position: 'absolute', marginTop: 0 }}
+            >
+              This is a dialog
+            </DialogContent>
+          </Dialog>
+        </Box>
+        <Box css={{ zIndex: 10000, backgroundColor: '$error4' }}>
+          <Dialog>
+            <DialogTrigger>
+              <Button>zIndex: 10000</Button>
+            </DialogTrigger>
+            <DialogContent
+              container={element}
+              overlayCss={{
+                backgroundColor: '$error4',
+                opacity: 0.9,
+                position: 'absolute',
+              }}
+              css={{ position: 'absolute', marginTop: 0 }}
+            >
+              This is a dialog
+            </DialogContent>
+          </Dialog>
+        </Box>
+      </Row>
+      <Row
+        css={{ position: 'relative', minHeight: '200px', minWidth: '100%' }}
+        centered
+        ref={setElement}
+      >
+        <Text>This area will now contain the dialog</Text>
+      </Row>
+    </Column>
   )
 }
