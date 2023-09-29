b3d445c801b7e6b12b6eb6631aeecc7470112ac0
Stuart Hendren <stuart@committed.io>
2023-09-29 16:12:47 +0000

    Clean up tests

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -1,5 +1,6 @@
-/** @type { import('@storybook/react-vite').StorybookConfig } */
-const config = {
+import type { StorybookConfig } from '@storybook/react-vite'
+
+const config: StorybookConfig = {
   stories: [
     '../stories/**/*.mdx',
     '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
